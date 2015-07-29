4b706a2c3424468853a285ec2ff579ef382d26bc
Morgan Miller <m.miller722@gmail.com>
2015-07-29 09:26:58 -0600

    make test ask for status code'

--- spec/controllers/poptarts_controller_spec.rb
+++ spec/controllers/poptarts_controller_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 describe PoptartsController do
   context '#index' do
     it 'returns all the poptarts' do
+      Poptart.create(flavor: 'strawberry', sprinkles: 'red')
+
       get :index, format: :json
+
+      expect(response).to have_http_status(:ok)
     end
   end
 end
